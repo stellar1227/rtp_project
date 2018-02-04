5754dfe8c6a2ec30009489de62f467ba51ac6442
stellar1227 <halfjin7@gmail.com>
2018-02-05 03:37:32 +0900

    공통레이어 작업중

--- pc_web/resources/src/js/common-ui.js
+++ pc_web/resources/src/js/common-ui.js
@@ -19,9 +19,63 @@
         }
 
     };
+
+    /*layer*/ //작업중
+    /*
+    * @description layerpop
+    * @param opt : Object
+    * @param opt.type : String (alert, confirm - default : null)
+    * @param opt.title : String (default null)
+    * @param opt.msg : String (defalut "")
+    * @param opt.contSelector : jQuery selector (ex - #id, .class / defalut null)
+    * @param opt.okCallBack : function
+    * @param opt.cancelCallBack : function
+    * @param opt.okBtnTxt : String (default "확인")
+    * @param opt.cancelBtnTxt : String (default "취소")
+    * */
+   /* function layerPop (opt){ 
+         this._opt = {
+          type : "alert",
+          title : null,
+          msg : "",
+          isBtn : true,
+          okBtnTxt : "확인",
+          cancelBtnTxt : "취소"
+        };
+        var _newOpt = $.extend(true, _opt, opt);
+        console.log(_newOpt);
+        this.init = function(){
+            this.setting();
+        };
+        this.setting = function(){
+            var optType = Object.prototype.toString.call(opt);
+            if(optType !== "[object Object]") throw "invalid type"; //객체인지 확인
+        };
+        this.addEvent = function(){
+
+        };
+        this._open = function(){
+
+        };
+        this._close = function(){
+
+        };
+        this.render = function(){
+
+        };
+        this.init();
+    };
+
+    layerPopup.prototype.alert = function(){
+      console.log(this._opt)
+    };*/
     //document.ready
     $(function(){
 
         gnb.init(); //gnb
+        /*var oc = new layerPop({
+            type : "alert",
+            msg :"알림입니다.",
+        });*/
     })
 })(jQuery);
